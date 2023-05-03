7ae28bebc75ecd175938e4f076b8f76d0ed3a017
Luca Lusso <lussoluca@gmail.com>
2023-05-03 09:55:53 +0200

    Test home

--- src/components/Hero3.jsx
+++ src/components/Hero3.jsx
@@ -92,8 +92,7 @@ export function Hero3() {
               AlessandriaScout
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              Benvenuta/o sul sito dello scoutismo alessandrino, promosso dai Gruppi AGESCI cittadini (AL1, AL2, AL3), dalla Comunità del Movimento Adulti Scout Cattolici Italiani (MASCI) e dagli Amici degli Scout di Alessandria.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
