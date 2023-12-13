b775d2bedbf40e0b68fd7c916ef32ac631609060
Luca Lusso <lussoluca@gmail.com>
2023-12-13 18:00:45 +0100

    Abilita SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '@/styles/tailwind.css'
 import { Metadata } from 'next'
 import { TITLE, DESCRIPTION } from '@/lib/constants'
 import React from 'react'
-import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body className="flex min-h-screen flex-col">{children}</body>
-      <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
