44b820dddb86858fa1f3a4c5a7c828f31a12518f
Luca Lusso <lussoluca@gmail.com>
2023-12-18 10:43:55 +0100

    Masconde le foto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Layout>
         <Hero />
-        <Photos />
+        {/* <Photos /> */}
         <Quote />
         {/*<Groups />*/}
         <Faqs />
