e5ceb1ff208dbbe651d081b1fef350b7f4c16352
Luca Lusso <lussoluca@gmail.com>
2024-12-10 22:36:41 +0100

    Modifica il margine del paragrafo dell'estratto del post per migliorare la spaziatura

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -25,26 +25,7 @@ export default async function Post(post: PostType) {
         </span>
         <DateFormatter dateString={post.date} />
       </time>
-      <p className="relative z-10 mt-2 text-sm">{post.excerpt}</p>
-      <div
-        aria-hidden="true"
-        className="relative z-10 mt-4 flex items-center text-sm font-medium text-scouting-purple"
-      >
-        Leggi l’articolo
-        <svg
-          viewBox="0 0 16 16"
-          fill="none"
-          aria-hidden="true"
-          className="ml-1 h-4 w-4 stroke-current"
-        >
-          <path
-            d="M6.75 5.75 9.25 8l-2.5 2.25"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
-      </div>
+      <p className="relative z-10 mt-2 mb-5 text-sm">{post.excerpt}</p>
     </article>
   )
 }
